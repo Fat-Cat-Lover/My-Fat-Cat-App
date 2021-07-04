ffba0a478bf6384b29d4d61eac341db996eb44b8
nekochao <nekoschao1216@lockists.com>
2021-07-05 01:10:18 +0800

    change onboard transition

--- src/pages/On-Boarding/navigation.tsx
+++ src/pages/On-Boarding/navigation.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
+import { CardStyleInterpolators, createStackNavigator } from '@react-navigation/stack';
 import { Step1 } from './pages/Step1/Step1';
 import { Step2 } from './pages/Step2/Step2';
 import { Step3 } from './pages/Step3/Step3';
@@ -7,7 +7,8 @@ import { Step3 } from './pages/Step3/Step3';
 const Stack = createStackNavigator();
 
 export const OnBoardStack = () => (
-  <Stack.Navigator screenOptions={{ headerShown: false }}>
+  <Stack.Navigator
+    screenOptions={{ headerShown: false, cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS }}>
     <Stack.Screen name="step1" component={Step1} />
     <Stack.Screen name="step2" component={Step2} />
     <Stack.Screen name="step3" component={Step3} />
